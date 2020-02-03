44bb6ac839a9d415406d1da46ab373ec9d417c3d
mihauman <mihauman1313@gmail.com>
2020-02-03 21:51:29 +0200

    Homework#1

---
+++ Homework1JS.js
@@ -0,0 +1,36 @@
+const carrot = 123.965;
+const egg = 15.678;
+const onion = 90.2345;
+const money = 500;
+
+maxPrice = Math.max(egg,carrot,onion);
+minPrice = Math.min(egg,carrot,onion);
+const sum = egg + carrot + onion;
+wholePart = Math.floor(egg,carrot,onion);
+rest = money - sum;
+middlePrice = sum/(egg,carrot,onion);
+thous = Math.ceil(sum / 100) * 100;
+
+if(minPrice % 2 !==0) {
+console.log(false)
+} else {
+console.log(true)
+}
+
+price = Math.random(100)*400;
+discount = Math.floor(Math.random()*100);
+sumDiscount = price*discount/100;
+sumPay = (price - (price*discount/100)).toFixed(2);
+lostProfit = (price/2) - sumDiscount;
+
+console.log(`Максимальна ціна: ${maxPrice}`);
+console.log(`Мінімальна ціна: ${minPrice}`);
+console.log(`Сума: ${sum}`);
+console.log(`Ціла частина вартості всіх товарів: ${wholePart}`);
+console.log(`Сума товарів округлених до сотень: ${thous}`);
+console.log(`Сдача з 500 грн: ${rest}`);
+console.log(`Середнє значення цін: ${middlePrice}`);
+console.log(`Ціна: ${price}`);
+console.log(`Знижка: ${discount}%`);
+console.log(`Ціна для оплати: ${sumPay}`);
+console.log(`Упущена вигода: ${lostProfit}`);
